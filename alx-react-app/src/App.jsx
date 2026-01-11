fc9e8f5450088a4b53ab4fa3aa088240bd3646f0
Safaa <safaaeltom@gmail.com>
2026-01-11 13:53:08 +0300

    Added UserProfile.jsx to components subfolder

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -35,6 +36,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <UserProfile 
+            name="Alice" 
+            age="25" 
+            bio="Loves hiking and photography" 
+        />
       <Footer />
     </>
   )
---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,11 @@
+function UserProfile(props) {
+    return (
+        <div>
+            <h1>{props.name}</h1>
+            <p>Age: {props.age}</p>
+            <p>Bio: {props.bio}</p>
+        </div>
+    );
+}
+
+export default UserProfile;
