e6f86ac5571c9e4a536cd3e3566a4a9f28342225
Safaa <safaaeltom@gmail.com>
2026-01-20 22:28:07 +0300

    Add alx-react-app-new React project to repo

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+function MainContent() {
+return (
+<main style={{ 
+  backgroundColor: 'purple',
+  fontWeight: 'bold',
+  height: '30px',
+  }}>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+);
+}
+
+export default MainContent;
