978162bea701155ff78ac76554b52cbc48db2b51
Safaa <safaaeltom@gmail.com>
2026-01-24 15:06:00 +0300

    Fulfilled all ALX requests

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,10 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '15px', backgrounColor:'purple'}}>
+    <nav style={{ padding: '15px', 
+    backgroundColor:'purple', 
+    display: 'flex', 
+    justifyContent: 'center'}}>
       <Link to="/" style= {{ margin: '0 10px' }}>Home</Link>
       <Link to="/about" style={{ margin: '0 10px' }}>About</Link>
       <Link to="/services" style={{ margin: '0 10px' }}>Services</Link>
@@ -12,3 +15,4 @@ function Navbar() {
 }
 
 export default Navbar;
+
