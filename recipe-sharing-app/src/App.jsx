33e82f436fd1a88c2ff9d0562001655cc732da99
Safaa <safaaeltom@gmail.com>
2026-01-25 02:46:38 +0300

    Added directory to repo

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,20 @@
-import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
+import AddRecipeForm from "./components/AddRecipeForm";
 
 function App() {
   return (
-      <div>
-        <RecipeList/>
-        <AddRecipeForm/>
-      </div>
+    <BrowserRouter>
+      <RecipeList />
+      <AddRecipeForm />
+
+      <Routes>
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
 
+export default App;
