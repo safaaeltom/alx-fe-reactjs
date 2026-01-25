69c93ba77bc1e3ce1535c0047f87515968dba7ce
Safaa <safaaeltom@gmail.com>
2026-01-25 18:32:43 +0300

    Updated the repo with filtered recipe function

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,33 +2,36 @@ import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
-  
-  searchTerm: '',
-  setSearchTerm: (term) => set({ searchTerm: term }),
-  
-  filteredRecipes: [],
-  filterRecipes: () => set(state => ({
-    filteredRecipes: state.recipes.filter(recipe =>
-      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
-    ),
-  }),
-),
-  
   addRecipe: (newRecipe) => 
     set((state) => ({ 
-      recipes: [...state.recipes, newRecipe] })),
+      recipes: [...state.recipes, newRecipe],
+     })),
 
+  setRecipes: (recipes) => set({ recipes }),
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+  
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
         recipe.id === updatedRecipe.id ? updatedRecipe : recipe
       ),
     })),
+  searchTerm: '',
+  setSearchTerm: (term) => set({  searchTerm: term}),
+  filteredRecipes: [],
+  filteredRecipes: () => set(state => ({
+  filteredRecipes: state.recipes.filter((recipe) =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+    ),
+  })),
+  
+  
+  
 
-  deleteRecipe: (id) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    })),
+  
 
-  setRecipes: (recipes) => set({ recipes }),
+  
 }));
