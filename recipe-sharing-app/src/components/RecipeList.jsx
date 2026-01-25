580e68226ab0216cf72444488f787bf19e27943f
Safaa <safaaeltom@gmail.com>
2026-01-25 18:48:08 +0300

    Added route and link to recipe list

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,5 @@
 import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom';
 
   const RecipeList = () => {
     const recipes = useRecipeStore(state => state.filteredRecipes);
@@ -7,7 +8,8 @@ import { useRecipeStore } from './recipeStore';
       <div>
         {filteredRecipes.map(recipe => (
           <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
+            <h3><Link to={`/recipe/${recipe.id}`}>
+            {recipe.title}</Link></h3>
             <p>{recipe.description}</p>
           </div>
         ))}
