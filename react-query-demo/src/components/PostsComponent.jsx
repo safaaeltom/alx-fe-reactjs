0d66343120c57200f23790c3736b06452bbb0185
Safaa <safaaeltom@gmail.com>
2026-02-23 13:13:37 +0300

    Added react-query-demo to main repo

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,35 @@
+import { useQuery } from '@tanstack/react-query';
+
+const fetchPosts = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  if (!res.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return res.json();
+};
+
+function PostsComponent() {
+  const { data, error, isLoading, refetch } = useQuery({
+  queryKey: ['posts'],
+  queryFn: fetchPosts,
+})
+
+  if (isLoading) return <p>Loading posts...</p>;
+  if (error) return <p>Error fetching posts: {error.message}</p>;
+
+  return (
+    <div>
+      <button onClick={() => refetch()}>Refetch Posts</button>
+      <ul>
+        {data.map(post => (
+          <li key={post.id}>
+            <strong>{post.title}</strong>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PostsComponent;
